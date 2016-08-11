d041e5950ac00076377c4b9285aa217a7fccd161
wocoburguesa <wocoburguesa@gmail.com>
2016-08-11 12:29:54 -0500

    update 1

--- server.js
+++ server.js
@@ -116,6 +116,77 @@ function CallMediaPipeline() {
     this.webRtcEndpoint = {};
 }
 
+function webRTCEndpointCallback(error, callerWebRtcEndpoint) {
+    if (error) {
+        pipeline.release();
+        return callback(error);
+    }
+
+    if (candidatesQueue[callerId]) {
+        while(candidatesQueue[callerId].length) {
+            var candidate = candidatesQueue[callerId].shift();
+            callerWebRtcEndpoint.addIceCandidate(candidate);
+        }
+    }
+
+    callerWebRtcEndpoint.on('OnIceCandidate', function(event) {
+        var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+        userRegistry.getById(callerId).ws.send(JSON.stringify({
+            id : 'iceCandidate',
+            candidate : candidate
+        }));
+    });
+
+    pipeline.create('WebRtcEndpoint', function(error, calleeWebRtcEndpoint) {
+        if (error) {
+            pipeline.release();
+            return callback(error);
+        }
+
+        if (candidatesQueue[calleeId]) {
+            while(candidatesQueue[calleeId].length) {
+                var candidate = candidatesQueue[calleeId].shift();
+                calleeWebRtcEndpoint.addIceCandidate(candidate);
+            }
+        }
+
+        calleeWebRtcEndpoint.on('OnIceCandidate', function(event) {
+            var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+            userRegistry.getById(calleeId).ws.send(JSON.stringify({
+                id : 'iceCandidate',
+                candidate : candidate
+            }));
+        });
+
+        callerWebRtcEndpoint.connect(calleeWebRtcEndpoint, function(error) {
+            if (error) {
+                pipeline.release();
+                return callback(error);
+            }
+
+            calleeWebRtcEndpoint.connect(callerWebRtcEndpoint, function(error) {
+                if (error) {
+                    pipeline.release();
+                    return callback(error);
+                }
+            });
+
+            self.pipeline = pipeline;
+            self.webRtcEndpoint[callerId] = callerWebRtcEndpoint;
+            self.webRtcEndpoint[calleeId] = calleeWebRtcEndpoint;
+            callback(null);
+        });
+    });
+}
+
+function mediaPipeLineCallback(error, pipeline) {
+    if (error) {
+        return callback(error);
+    }
+
+    pipeline.create('WebRtcEndpoint', webRTCEndpointCallback);
+}
+
 CallMediaPipeline.prototype.createPipeline = function(callerId, calleeId, ws, callback) {
     var self = this;
     getKurentoClient(function(error, kurentoClient) {
@@ -123,74 +194,7 @@ CallMediaPipeline.prototype.createPipeline = function(callerId, calleeId, ws, ca
             return callback(error);
         }
 
-        kurentoClient.create('MediaPipeline', function(error, pipeline) {
-            if (error) {
-                return callback(error);
-            }
-
-            pipeline.create('WebRtcEndpoint', function(error, callerWebRtcEndpoint) {
-                if (error) {
-                    pipeline.release();
-                    return callback(error);
-                }
-
-                if (candidatesQueue[callerId]) {
-                    while(candidatesQueue[callerId].length) {
-                        var candidate = candidatesQueue[callerId].shift();
-                        callerWebRtcEndpoint.addIceCandidate(candidate);
-                    }
-                }
-
-                callerWebRtcEndpoint.on('OnIceCandidate', function(event) {
-                    var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
-                    userRegistry.getById(callerId).ws.send(JSON.stringify({
-                        id : 'iceCandidate',
-                        candidate : candidate
-                    }));
-                });
-
-                pipeline.create('WebRtcEndpoint', function(error, calleeWebRtcEndpoint) {
-                    if (error) {
-                        pipeline.release();
-                        return callback(error);
-                    }
-
-                    if (candidatesQueue[calleeId]) {
-                        while(candidatesQueue[calleeId].length) {
-                            var candidate = candidatesQueue[calleeId].shift();
-                            calleeWebRtcEndpoint.addIceCandidate(candidate);
-                        }
-                    }
-
-                    calleeWebRtcEndpoint.on('OnIceCandidate', function(event) {
-                        var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
-                        userRegistry.getById(calleeId).ws.send(JSON.stringify({
-                            id : 'iceCandidate',
-                            candidate : candidate
-                        }));
-                    });
-
-                    callerWebRtcEndpoint.connect(calleeWebRtcEndpoint, function(error) {
-                        if (error) {
-                            pipeline.release();
-                            return callback(error);
-                        }
-
-                        calleeWebRtcEndpoint.connect(callerWebRtcEndpoint, function(error) {
-                            if (error) {
-                                pipeline.release();
-                                return callback(error);
-                            }
-                        });
-
-                        self.pipeline = pipeline;
-                        self.webRtcEndpoint[callerId] = callerWebRtcEndpoint;
-                        self.webRtcEndpoint[calleeId] = calleeWebRtcEndpoint;
-                        callback(null);
-                    });
-                });
-            });
-        });
+        kurentoClient.create('MediaPipeline', mediaPipeLineCallback);
     })
 }
 
